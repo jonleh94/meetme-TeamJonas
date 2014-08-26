ad79ca75b730687d04d9481cdadacc551989e19d
sparti <joern.schimmelpfeng@gmail.com>
2014-08-26 16:27:07 +0200

    updated rest api

--- pictureserver/src/main/java/de/dhbw/pictureshow/domain/User.java
+++ pictureserver/src/main/java/de/dhbw/pictureshow/domain/User.java
@@ -2,11 +2,13 @@ package de.dhbw.pictureshow.domain;
 
 
 import javax.persistence.Entity;
+import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  *
  */
 @Entity
+@XmlRootElement
 public class User extends PersistentObject {
   private String name;
 
--- pictureserver/src/main/java/de/dhbw/pictureshow/rest/UserService.java
+++ pictureserver/src/main/java/de/dhbw/pictureshow/rest/UserService.java
@@ -3,35 +3,36 @@ package de.dhbw.pictureshow.rest;
 import de.dhbw.pictureshow.database.dao.UserDao;
 import de.dhbw.pictureshow.domain.User;
 import de.dhbw.pictureshow.domain.UuidId;
+import groovy.lang.Singleton;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.websocket.server.PathParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import java.util.Collection;
 
 /**
  *
  */
-@Path("/rest/user")
+@Path("/api/user")
+@Produces({"text/xml", "application/json"})
+@Singleton
 public class UserService {
   private static final Logger log = LoggerFactory.getLogger(UserService.class);
 
-  @EJB
-  UserDao userDao;
+  @Inject UserDao userDao;
 
   @Path("/list")
   @GET
-  public Collection<User> list(){
+  public Collection<User> list() {
     log.debug("List users");
     return userDao.list();
   }
 
-  @Path("/get/{id}")
-  @GET
-  public User getUser(@PathParam("id") String id){
-    return userDao.findById(UuidId.fromString(id));
-  }
 }
