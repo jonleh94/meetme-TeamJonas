ee17b33c5da9d075a2b2ecabf6e8181daddb00ac
joern.schimmelpfeng <joern.schimmelpfeng@gmail.com>
2015-08-30 18:36:17 +0200

    added android app

---
+++ meetme-app/src/main/java/de/dhbw/meetme/HelloActivity.java
@@ -0,0 +1,23 @@
+package de.dhbw.meetme;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+import org.hello.R;
+
+public class HelloActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hello_layout);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        TextView textView = (TextView) findViewById(R.id.text_view);
+        textView.setText("Hello world!");
+    }
+
+}
